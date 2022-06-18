1da6b4afd2858a2201ee97a0b461b0b55fd8c2d5
Ismael-r-pithan <pithanismael@gmail.com>
2022-06-18 17:16:09 -0300

    first commit

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,51 @@
+/* eslint-disable prettier/prettier */
+import { User } from "../../model/User";
+import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private users: User[];
+
+  private static INSTANCE: UsersRepository;
+
+  private constructor() {
+    this.users = [];
+  }
+
+  public static getInstance(): UsersRepository {
+    if (!UsersRepository.INSTANCE) {
+      UsersRepository.INSTANCE = new UsersRepository();
+    }
+
+    return UsersRepository.INSTANCE;
+  }
+
+  create({ name, email }: ICreateUserDTO): User {
+    const newUser = new User();
+    Object.assign(newUser, {
+      email,
+      name,
+    });
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  findById(id: string): User | undefined {
+    return this.users.find((user) => user.id === id);
+  }
+
+  findByEmail(email: string): User | undefined {
+    return this.users.find((user) => user.email === email);
+  }
+
+  turnAdmin(receivedUser: User): User {
+    const user = this.users.find((user) => receivedUser.id === user.id);
+    user.admin = true;
+    user.updated_at = new Date();
+    return user;
+  }
+
+  list(): User[] {
+    return this.users;
+  }
+}
+export { UsersRepository };
